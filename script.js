cad39d7389b7347cc8ad137e227f474a3489b92a
jhoan franco <sebastianfrancogarces1322@gmail.com>
2025-10-16 10:01:44 -0500

    cree el script

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("vamos a jugar piedra, papel o tijera 🪨📄✂️")
+let jugador=prompt("cual elijes")
+
+
+if(jugador === "🪨" ||jugador === "📄" ||jugador === "✂️"){
+
+
+}else{
+    alert ("Escogiste ")
+}
+
+const clase = "clase aplicaciones II"
+console.log(clase)
+clase = 4
+console.log(clase)
+
