ec7fd191a4c142f9b6aeaabe330bf950257c5c7f
jhoan franco <sebastianfrancogarces1322@gmail.com>
2025-10-23 07:38:50 -0500

    modifique el script

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 alert("vamos a jugar piedra, papel o tijera 🪨📄✂️")
-let jugador=prompt("cual elijes")
+let jugador=prompt("Jugador 1 cual elijes")
+let jugador2=prompt("Jugador 2 cual elijes")
 
 
 if(jugador === "🪨" ||jugador === "📄" ||jugador === "✂️"){
 
+    if((jugador === "🪨" && jugador2 ==="✂️")||
+       (jugador === "📄" && jugador2 ==="🪨")||
+       (jugador === "✂️" && jugador2 ==="📄")
+    
+     )alert ("Gane")
 
 }else{
-    alert ("Escogiste ")
+    alert ("Escogiste una opcion invalida 👨‍🦽")
 }
 
-const clase = "clase aplicaciones II"
-console.log(clase)
-clase = 4
-console.log(clase)
+
+
 
